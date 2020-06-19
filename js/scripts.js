e29871c8ba859dce1d462e3525a9447cd8cf70c7
apul8377 <apulvers@my.chemeketa.edu>
2020-06-19 07:31:19 -0700

    fixed some spacing issues, and the link in the readme

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $("document").ready(function(){
     let inputValue = $("input#userNumber").val();
     for(i = 0; i <= inputValue; i++){
       let makeString = i.toString();
-      makeString = makeString.replace("3", "Won't you be my neighbor? ").replace("2", "Boop! ").replace("1", "Beep! ");
+      makeString = makeString.replace("3", " Won't you be my neighbor? ").replace("2", "Boop! ").replace("1", "Beep! ");
         console.log(makeString);
         if(inputValue >= 999 || inputValue < 1){
           $("#outputText").text(inputValue + ". Please keep your input between 1-999.");
