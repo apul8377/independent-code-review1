2a85813f39283f8a1a74c373f34d836a53afe26d
apul8377 <apulvers@my.chemeketa.edu>
2020-06-21 13:35:14 -0700

    Fixed the user interface logic by removing the extra number in the output. Cleaned up the UI a little more.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,44 @@
-//User Interface Logic
-$("document").ready(function(){
-  $("form#userInput").submit(function(event){
-    event.preventDefault();
-    let inputValue = $("input#userNumber").val();
-    for(i = 0; i <= inputValue; i++){
-      let makeString = i.toString();
-      makeString = makeString.replace("3", " Won't you be my neighbor? ").replace("2", "Boop! ").replace("1", "Beep! ");
-        console.log(makeString);
-        if(inputValue >= 999 || inputValue < 1){
-          $("#outputText").text(inputValue + ". Please keep your input between 1-999.");
-        }else{
-      $("#outputText").text(makeString);
-        }
-      $("input#userNumber").val(" ");
+//Business logic
+output= []; 
+let str = 0;
+let newStr = 0;
+
+  function replace() {
+    if (str.includes(3) == 1) {
+      newStr = str.replace(str, ' Wont you be my neighbor? ');
+    } else if (str.includes(2) == 1) {
+      newStr = str.replace(str, ' Boop!');
+    } else if (str.includes(1) == 1) {
+      newStr = str.replace(str, ' Beep!');
+    } else {
+      newStr = str;
+    }
+  }
+  
+
+
+  //User Interface Logic      
+  $("document").ready(function() {
+    $("form#userInput").submit(function(event) {
+      event.preventDefault();
+      let inputValue = $("input#userNumber").val();
+      for (i = 0; i <= inputValue; i++) {
+        str = i.toString();
+          replace();
+        output += newStr; 
+        
+          
+          $("#outputText").text(output);
+          $("input#userNumber").val(" ");
       }
-  });
-});
+       if (inputValue >= 999 || inputValue <= 0) {
+          $("#outputText").text(inputValue + ". Please keep your input between 1-999.");
+        }
+      output = " ";
+      
+      
+      });
+    });
+  
+
+  
