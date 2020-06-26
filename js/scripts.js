2291491da29bea61dcbc3570601b19d0f7f83b67
apul8377 <apulvers@my.chemeketa.edu>
2020-06-25 20:49:15 -0700

    removed global variables and refactored code. simplified the replace function

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,41 @@
 //Business logic
-output= []; 
-let str = 0;
-let newStr = 0;
 
-  function replace() {
-    if (str.includes(3) == 1) {
-      newStr = str.replace(str, ' Wont you be my neighbor? ');
-    } else if (str.includes(2) == 1) {
-      newStr = str.replace(str, ' Boop!');
-    } else if (str.includes(1) == 1) {
-      newStr = str.replace(str, ' Beep!');
-    } else {
-      newStr = str;
-    }
+function replace(userInput) {
+  str = [];
+  for (i = 0; i <= userInput; i++) {
+    str.push(i);
+    let newStr = str;
+    // if (str.includes(3) == 1) {
+    //   str.splice(3, 1, "Won't you be my neighbor?");
+    // }
+    // if (str.includes(2) == 1) {
+    //   str.splice(2, 1, "Beep!");
+    // }
+    // if (str.includes(1) == 1) {
+    //   str.splice(1, 1, "Boop!");
+    // }
+
+    return str;
   }
-  
+}
 
+//User Interface Logic
+$("document").ready(function () {
+  $("form#userInput").submit(function (event) {
+    event.preventDefault();
+    inputValue = $("input#userNumber").val();
+    let stringReplaced = replace(inputValue);
+    console.log(stringReplaced);
+    output = [];
+    output += stringReplaced;
 
-  //User Interface Logic      
-  $("document").ready(function() {
-    $("form#userInput").submit(function(event) {
-      event.preventDefault();
-      let inputValue = $("input#userNumber").val();
-      for (i = 0; i <= inputValue; i++) {
-        str = i.toString();
-          replace();
-        output += newStr; 
-        
-          
-          $("#outputText").text(output);
-          $("input#userNumber").val(" ");
-      }
-       if (inputValue >= 999 || inputValue <= 0) {
-          $("#outputText").text(inputValue + ". Please keep your input between 1-999.");
-        }
-      output = " ";
-      
-      
-      });
-    });
-  
+    $("#outputText").text(output);
+    $("input#userNumber").val(" ");
 
-  
+    if (inputValue >= 999 || inputValue <= 0) {
+      $("#outputText").text(
+        inputValue + ". Please keep your input between 1-999."
+      );
+    }
+  });
+});
